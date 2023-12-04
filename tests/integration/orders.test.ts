f42ec343025795084cd56c5e3e456f480e3e34be
GDMeira <gabrieldega970@gmail.com>
2023-12-04 01:56:51 -0300

    feat: test to get orders/code

---
+++ tests/integration/orders.test.ts
@@ -0,0 +1,33 @@
+import httpStatus from 'http-status';
+import supertest from 'supertest';
+import app, { close, init } from '@/app';
+import { prisma } from '@/config';
+import { cleanDb } from '../helpers';
+
+const server = supertest(app);
+
+beforeAll(async () => {
+  await init();
+});
+
+beforeEach(async () => {
+  await cleanDb();
+});
+
+afterAll(async () => {
+  await close();
+});
+
+describe('GET /orders/code', () => {
+  it('should respond with status 200 and the code of next order', async () => {
+    const code = await prisma.order.aggregate({
+      _max: { id: true },
+    });
+
+    const response = await server.get('/orders/code');
+
+    expect(response.status).toBe(httpStatus.OK);
+    // eslint-disable-next-line no-underscore-dangle
+    expect(response.body).toEqual({ code: code._max });
+  });
+});
